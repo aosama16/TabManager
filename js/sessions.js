bc6966f3a9b35f7f371e5eea59b22e27778868f4
Abdelrahman Osama <abdelrahman.osama93@gmail.com>
2019-05-30 00:41:25 +0200

    [ver0.3] Added confirmation to deleting tabs/groups

--- js/sessions.js
+++ js/sessions.js
@@ -19,19 +19,23 @@ let app = new Vue({
             return `https://www.google.com/s2/favicons?domain=${new URL(tab.url).hostname}`;
         },
         deleteTab(groupID, tabID){
-            let groupIDX = this.state.groups.findIndex(group => group.id == groupID);
-            targetGroup = this.state.groups[groupIDX];
-            
-            let tabIDX = targetGroup.tabs.findIndex(tab => tab.id == tabID);
-            targetGroup.tabs.splice(tabIDX, 1);
-            
-            // Delete group if tabs are empty - MAY KEEP LATER if it is considere a collection
-            if(targetGroup.tabs.length == 0)
-                this.state.groups.splice(groupIDX, 1)
+            if(confirm("Do you really want to delete this tab?")){
+                let groupIDX = this.state.groups.findIndex(group => group.id == groupID);
+                targetGroup = this.state.groups[groupIDX];
+                
+                let tabIDX = targetGroup.tabs.findIndex(tab => tab.id == tabID);
+                targetGroup.tabs.splice(tabIDX, 1);
+                
+                // Delete group if tabs are empty - MAY KEEP LATER if it is considere a collection
+                if(targetGroup.tabs.length == 0)
+                    this.state.groups.splice(groupIDX, 1);
+            }
         },
         deleteGroup(groupID){
-            let groupIDX = this.state.groups.findIndex(group => group.id == groupID);
-            this.state.groups.splice(groupIDX, 1)
+            if(confirm("Do you really want to delete this group?")){
+                let groupIDX = this.state.groups.findIndex(group => group.id == groupID);
+                this.state.groups.splice(groupIDX, 1);
+            }
         },
         saveID(event){
             this.movedID = event.item.attributes['data-id'].textContent;
